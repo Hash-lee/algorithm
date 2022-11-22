2ab1c56a0dcdfd87c0617e9f323c1f8b7be45b54
Heruing <82349452+Heruing@users.noreply.github.com>
2022-11-22 15:53:18 +0900

    [Gold V] Title: 연속합 2, Time: 256 ms, Memory: 41588 KB -BaekjoonHub

--- 백준/Gold/13398. 연속합 2/연속합 2.py
+++ 백준/Gold/13398. 연속합 2/연속합 2.py
@@ -1,28 +1,20 @@
 import sys
 
 N = int(sys.stdin.readline())
-blank = []
 nums = list(map(int, sys.stdin.readline().split()))
-blank.append(nums[0])
 
-for idx in range(1, N):
-    if nums[idx] < 0 or blank[-1] < 0:
-        blank.append(nums[idx])
-    else:
-        blank[-1] += nums[idx]
-
-if max(blank) <= 0:
-    print(max(blank))
+if max(nums) <= 0:
+    print(max(nums))
     exit()
 
 
-L = len(blank)
+L = len(nums)
 dp = [[0, 0] for _ in range(L + 1)]
 
 mx = -1
 for idx in range(1, L + 1):
-    dp[idx][0] = max(dp[idx - 1][0] + blank[idx - 1], blank[idx - 1])
-    dp[idx][1] = max(dp[idx - 1][0], dp[idx - 1][1] + blank[idx - 1])
+    dp[idx][0] = max(dp[idx - 1][0] + nums[idx - 1], nums[idx - 1])
+    dp[idx][1] = max(dp[idx - 1][0], dp[idx - 1][1] + nums[idx - 1])
 
 
 for idx in range(1, L + 1):
