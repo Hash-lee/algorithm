3dd56c7a471bbce249219e424de0533c59fbe44c
Heruing <82349452+Heruing@users.noreply.github.com>
2022-12-02 20:14:06 +0900

    [Silver I] Title: 데스 나이트, Time: 128 ms, Memory: 32468 KB -BaekjoonHub

--- 백준/Silver/16948. 데스 나이트/데스 나이트.py
+++ 백준/Silver/16948. 데스 나이트/데스 나이트.py
@@ -2,24 +2,20 @@ import sys
 from collections import deque
 
 N = int(sys.stdin.readline())
-Sr, Sc, Fr, Fc = list(map(int, sys.stdin.readline().split()))
-dir_r = [-2, -2, 0, 0, 2, 2]
-dir_c = [-1, 1, -2, 2, -1, 1]
-visit = [[1] * N for _ in range(N)]
+Sr, Sc, Fr, Fc = map(int, sys.stdin.readline().split())
+D = [(-2, -1), (-2, 1), (0, -2), (0, 2), (2, -1), (2, 1)]
+V = [[1] * N for _ in range(N)]
 Q = deque()
-Q.append((Sr, Sc, 1))
-visit[Sr][Sc] = 0
+Q.append((Sr, Sc, 0))
+V[Sr][Sc] = 0
 while Q:
     r, c, t = Q.popleft()
-    for k in range(6):
-        nr = r + dir_r[k]
-        nc = c + dir_c[k]
-        if 0 <= nr < N and 0 <= nc < N and visit[nr][nc]:
+    new = tuple(map(lambda x: (r + x[0], c + x[1], t + 1), D))
+    for (nr, nc, t) in new:
+        if 0 <= nr < N and 0 <= nc < N and V[nr][nc]:
             if (nr, nc) == (Fr, Fc):
                 print(t)
                 exit()
-            else:
-                visit[nr][nc] = 0
-                Q.append((nr, nc, t + 1))
-
+            V[nr][nc] = 0
+            Q.append((nr, nc, t))
 print(-1)
