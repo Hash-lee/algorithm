4f8f0d0c33ae67fe8a943b53231c906d2eed038d
Heruing <82349452+Heruing@users.noreply.github.com>
2022-12-12 12:15:00 +0900

    [Silver I] Title: 부분수열의 합, Time: 748 ms, Memory: 88840 KB -BaekjoonHub

--- 백준/Silver/14225. 부분수열의 합/부분수열의 합.py
+++ 백준/Silver/14225. 부분수열의 합/부분수열의 합.py
@@ -1,23 +1,16 @@
 import sys
+from itertools import combinations
 
 N = int(sys.stdin.readline())
-numbers = list(map(int, sys.stdin.readline().split()))
-numbers.sort()
 
+ipts = tuple(map(int, sys.stdin.readline().split()))
+combs = sorted(tuple(set(((sum(s) for r in range(1, N + 1) for s in tuple(combinations(ipts, r)))))))
 
-arr = [1] * (2**20 + 1)
-arr[0] = 0
-arr[numbers[0]] = 0
-for number in numbers[1:]:
-    temps = [0]
-    for i in range(2**20 + 1 - number):
-        if not arr[i]:
-            temps.append(i)
-    for temp in temps:
-        arr[temp + number] = 0
-
-
-for idx in range(1, 2**20 + 1):
-    if arr[idx]:
-        print(idx)
+i = 0
+for n in combs:
+    i += 1
+    if n != i:
+        print(i)
         exit()
+
+print(i + 1)
