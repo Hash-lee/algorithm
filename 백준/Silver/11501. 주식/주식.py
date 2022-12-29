439e363bec540346fd3cc40ddf88adfd6708fdfa
Heruing <82349452+Heruing@users.noreply.github.com>
2022-12-29 15:19:58 +0900

    [Silver II] Title: 주식, Time: 3204 ms, Memory: 166204 KB -BaekjoonHub

--- 백준/Silver/11501. 주식/주식.py
+++ 백준/Silver/11501. 주식/주식.py
@@ -3,11 +3,10 @@ import sys
 T = int(sys.stdin.readline())
 for tc in range(1, T + 1):
     N = int(sys.stdin.readline())
-    prices = list(map(int, sys.stdin.readline().split()))[::-1]
+    prices = list(map(int, sys.stdin.readline().split()))
     income = 0
-    mx = prices[0]
-    for idx in range(1, N):
-        price = prices[idx]
-        if price < mx: income += mx-price
-        else: mx = price
+    mx = prices[-1]
+    for idx in range(N-2, -1,-1):
+        if prices[idx] < mx: income += mx-prices[idx]
+        elif prices[idx] > mx: mx = prices[idx]
     print(income)
