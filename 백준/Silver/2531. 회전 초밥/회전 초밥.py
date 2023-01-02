f09c710143ca69da48e726c4c95d84deade04acf
Heruing <82349452+Heruing@users.noreply.github.com>
2023-01-02 09:37:13 +0900

    [Silver I] Title: 회전 초밥, Time: 64 ms, Memory: 33024 KB -BaekjoonHub

--- 백준/Silver/2531. 회전 초밥/회전 초밥.py
+++ 백준/Silver/2531. 회전 초밥/회전 초밥.py
@@ -1,6 +1,22 @@
 import sys
 
-N,d,k,c=map(int, input().split())
-l=list(map(int, sys.stdin.readlines()))
-l+=l[:k]
-print(max([len(set(l[i:i+k])|set([c])) for i in range(N)]))
+N, d, k, c = map(int, input().split())
+sushi = list(map(int, sys.stdin.readlines()))
+include = [0] * (d + 1)
+include[c] = 100000
+var = 1
+for idx in range(k):
+    if not include[sushi[idx]]:
+        var += 1
+    include[sushi[idx]] += 1
+mx = var
+for s in range(1, N):
+    e = (s + k - 1) % N
+    if not include[sushi[e]]:
+        var += 1
+    include[sushi[e]] += 1
+    include[sushi[s - 1]] -= 1
+    if not include[sushi[s - 1]]:
+        var -= 1
+    mx = max(mx, var)
+print(mx)
