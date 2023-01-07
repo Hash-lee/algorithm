1673e87121a5d6893d9b1e79ae23d1bb2d2c9628
Heruing <82349452+Heruing@users.noreply.github.com>
2023-01-07 17:51:49 +0900

    [Silver III] Title: 수강신청, Time: 332 ms, Memory: 102360 KB -BaekjoonHub

--- 백준/Silver/13414. 수강신청/수강신청.py
+++ 백준/Silver/13414. 수강신청/수강신청.py
@@ -4,7 +4,7 @@ K, L = map(int, sys.stdin.readline().split())
 check = set()
 aligned = []
 
-applier = list(map(lambda x: x.strip(), sys.stdin.readlines()))
+applier = tuple(map(lambda x: x.strip(), sys.stdin.readlines()))
 for idx in range(-1, -L - 1, -1):
     if not applier[idx] in check:
         check.add(applier[idx])
