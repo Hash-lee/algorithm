dac4a4434ccf69981ded496152ed439c26c0076f
Heruing <82349452+Heruing@users.noreply.github.com>
2023-01-08 11:46:32 +0900

    [Gold V] Title: 상범 빌딩, Time: 116 ms, Memory: 34156 KB -BaekjoonHub

--- 백준/Gold/6593. 상범 빌딩/상범 빌딩.py
+++ 백준/Gold/6593. 상범 빌딩/상범 빌딩.py
@@ -1,21 +1,23 @@
 import sys
 from collections import deque
 
-dr = [0, 1, 0, -1, 0, 0]
-dc = [1, 0, -1, 0, 0, 0]
-dh = [0, 0, 0, 0, 1, -1]
+dr = [0, 0, 0, 0, 1, -1]
+dc = [0, 0, 1, -1, 0, 0]
+dh = [1, -1, 0, 0, 0, 0]
+
+
 def BFS(Q):
     h, r, c, t = Q[0]
-    building[h][r][c] = '#'
+    building[h][r][c] = "#"
     while Q:
         h, r, c, t = Q.popleft()
         for k in range(6):
-            nh, nr, nc, nt = h + dh[k], r + dr[k], c + dc[k], t + 1
+            nh, nr, nc = h + dh[k], r + dr[k], c + dc[k]
             if 0 <= nh < H and 0 <= nr < R and 0 <= nc < C:
-                if building[nh][nr][nc] == '.':
-                    building[nh][nr][nc] = '#'
-                    Q.append((nh, nr, nc, nt))
-                elif building[nh][nr][nc] == 'E':
+                if building[nh][nr][nc] == ".":
+                    building[nh][nr][nc] = "#"
+                    Q.append((nh, nr, nc, t + 1))
+                elif building[nh][nr][nc] == "E":
                     return t
     return 0
 
@@ -24,8 +26,8 @@ def start(H, R, C):
     for h in range(H):
         for r in range(R):
             for c in range(C):
-                if building[h][r][c] == 'S':
-                    return (h, r, c, 1)
+                if building[h][r][c] == "S":
+                    return BFS(deque([(h, r, c, 1)]))
 
 
 while True:
@@ -37,6 +39,5 @@ while True:
         building.append([list(sys.stdin.readline().rstrip()) for _ in range(R)])
         sys.stdin.readline()
 
-    Q = deque([start(H, R, C)])
-    t = BFS(Q)
+    t = start(H, R, C)
     print(f"Escaped in {t} minute(s).") if t else print("Trapped!")
