66f9867c907a6a68bc5efb264ef9eb10b53563d5
Heruing <s_1012@naver.com>
2023-03-21 02:14:28 +0900

    [level 2] Title: 빛의 경로 사이클, Time: 969.02 ms, Memory: 39.7 MB -BaekjoonHub

--- 프로그래머스/lv2/86052. 빛의 경로 사이클/빛의 경로 사이클.py
+++ 프로그래머스/lv2/86052. 빛의 경로 사이클/빛의 경로 사이클.py
@@ -1,8 +1,10 @@
 def solution(grid):
     answer = []
-    visited = [[[0]* 4 for _ in range(len(grid[0]))] for __ in range(len(grid))]
-    w, h = len(grid[0]), len(grid)
-    dr = [1, 0, -1, 0]
+    w = len(grid[0])
+    h = len(grid)
+
+    visited = [[[0] * 4 for _ in range(len(grid[0]))] for __ in range(len(grid))]
+    dr = [-1, 0, 1, 0]
     dc = [0, 1, 0, -1]
     for ir in range(h):
         for ic in range(w):
@@ -12,12 +14,14 @@ def solution(grid):
                     while Q:
                         r, c, d = Q.pop()
                         nr, nc, nd = (h + r + dr[d]) % h, (w + c + dc[d]) % w, d
-                        if grid[nr][nc] == "L": nd = (d + 3) % 4
-                        if grid[nr][nc] == "R": nd = (d + 1) % 4
+                        if grid[nr][nc] == "L":
+                            nd = (d + 3) % 4
+                        if grid[nr][nc] == "R":
+                            nd = (d + 1) % 4
                         if visited[nr][nc][nd]:
                             answer.append(visited[r][c][d])
-                        else: 
+                        else:
                             visited[nr][nc][nd] = visited[r][c][d] + 1
                             Q.append((nr, nc, nd))
-    answer.sort()                        
+    answer.sort()
     return answer
