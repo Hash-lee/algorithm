a431001591278633c152cbd9585a4685a00778f6
Heruing <s_1012@naver.com>
2023-05-16 07:56:30 +0900

    [Silver III] Title: 파도반 수열, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.py
@@ -0,0 +1,11 @@
+N = int(input())
+
+P = [1, 1, 1, 2, 2, 3, 4, 5, 7, 9]
+l = 10
+
+for n in range(N):
+    x = int(input())
+    while l < x:
+        l += 1
+        P.append(P[l-4]+P[l-3])
+    print(P[x-1])
