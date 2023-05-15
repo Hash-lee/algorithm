cc73f2e71238a9ea2e6f37e305f26ef0917bfc23
Heruing <s_1012@naver.com>
2023-05-16 07:58:14 +0900

    [Silver III] Title: N과 M (1), Time: 112 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -0,0 +1,12 @@
+def comb(n, m, s=''):
+    if m == 0: print(s)
+    else:
+        for i in range(1, N+1):
+            if V[i] == 0:
+                V[i] = 1
+                comb(n, m-1, s + str(i) + ' ')
+                V[i] = 0
+
+N, M = map(int, input().split())
+V = [0] * (N+1)
+comb(N, M)
