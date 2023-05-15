8189620bbe28f1ac43e1d357c715714e88033ec5
Heruing <s_1012@naver.com>
2023-05-16 07:53:06 +0900

    [Bronze II] Title: 크로스워드 만들기, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2804. 크로스워드 만들기/크로스워드 만들기.py
@@ -0,0 +1,20 @@
+a,b = input().split()
+
+ida = -1
+idb = -1
+
+for i in range(len(a)):
+    for j in range(len(b)):
+        if a[i] == b[j]:
+            ida = i
+            idb = j
+            break
+    if ida != -1:
+        break
+
+
+for k in range(len(b)):
+    if k == idb:
+        print(a)
+    else:
+        print('{}{}{}'.format('.'*ida, b[k], '.'*(len(a)-ida-1)))
