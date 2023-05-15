8b7c6ec173d2cf73c9f431c05e8e6707f69d60ee
Heruing <s_1012@naver.com>
2023-05-16 07:54:25 +0900

    [Silver I] Title: 나이트의 이동, Time: 716 ms, Memory: 218092 KB -BaekjoonHub

---
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.py
@@ -0,0 +1,36 @@
+dr = [2, -1, 2, 1, -2, 1, -1, -2]
+ds = [-1, 2, 1, 2, 1, -2, -2, -1]
+def knight(r, c, n):
+    for k in range(8):
+        nr = r + dr[k]
+        nc = c + ds[k]
+        if 0 <= nr < L and 0 <= nc < L and V[nr][nc] == 0:
+            inid = abs(er-sr) +abs(ec-sc)
+            nd = abs(er-nr) + abs(ec-nc)
+            if nd < inid or nd < 5:
+                V[nr][nc] = n+1
+                Q.append((nr, nc, n+1))
+
+
+for tc in range(1, int(input())+1):
+    L = int(input())
+    sr, sc = map(int, input().split())
+    er, ec = map(int, input().split())
+
+    if (sr, sc) == (er, ec): print('0')
+    else:
+        Q = []
+        Q.append((sr, sc, 1))
+        V = [[0]*L for _ in range(L)]
+
+        idx = -1
+        while True:
+            idx += 1
+            r = Q[idx][0]
+            c = Q[idx][1]
+            n = Q[idx][2]
+
+            if r == er and c == ec:
+                print(n-1)
+                break
+            knight(r, c, n)
